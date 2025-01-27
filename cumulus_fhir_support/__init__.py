ec49ecda7111990abbbec11e65a336e655c944ec
Michael Terry <michael.terry@childrens.harvard.edu>
2025-01-27 11:59:34 -0500

    Fix ruff complaints

--- cumulus_fhir_support/__init__.py
+++ cumulus_fhir_support/__init__.py
@@ -2,5 +2,5 @@
 
 __version__ = "1.3.0"
 
-from .json import list_multiline_json_in_dir, read_multiline_json, read_multiline_json_from_dir
+from .ml_json import list_multiline_json_in_dir, read_multiline_json, read_multiline_json_from_dir
 from .schemas import pyarrow_schema_from_rows
